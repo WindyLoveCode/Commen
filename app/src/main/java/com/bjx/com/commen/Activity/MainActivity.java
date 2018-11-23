f612f3e74bfaeb2fac9185a74528dd4d83f44e1f
WindyLoveCode <994856229@qq.com>
2018-11-23 16:27:11 +0800

    通用工具集合

--- app/src/main/java/com/bjx/com/commen/Activity/MainActivity.java
+++ app/src/main/java/com/bjx/com/commen/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bjx.com.commen;
+package com.bjx.com.commen.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bjx.com.commen.R;
 
 public class MainActivity extends AppCompatActivity {
 
