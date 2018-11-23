f49cfd34dc9003ec5c8f1014206c9b75356cdfa4
WindyLoveCode <994856229@qq.com>
2018-11-23 16:23:33 +0800

    通用工具集合

---
+++ app/src/main/java/com/bjx/com/commen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bjx.com.commen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
