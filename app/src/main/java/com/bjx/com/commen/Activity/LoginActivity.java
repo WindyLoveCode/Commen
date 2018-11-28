a58d3012767f2a69cab7025b158f0a9c8a520873
WindyLoveCode <994856229@qq.com>
2018-11-28 10:28:39 +0800

    添加登录界面

--- app/src/main/java/com/bjx/com/commen/Activity/LoginActivity.java
+++ app/src/main/java/com/bjx/com/commen/Activity/LoginActivity.java
@@ -2,14 +2,13 @@ package com.bjx.com.commen.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import com.bjx.com.commen.R;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
